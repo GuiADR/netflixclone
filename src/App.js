4219e38fccc9d73a3983bc0e529bb5c3ef64bb6b
Guilherme Rodrigues <guilherme.a.d.rodrigues@gmail.com>
2020-10-25 22:07:57 +0000

    Aplicação faltando ultimos ajustes

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ export default () => {
       </section>
 
       <footer>
-        Feito com <span role="img" aria-label="coração">❤️</span> pela B7Web<br/>
+        Feito com inspirado por uma aula da B7Web<br/>
         Direitos de imagem para Netflix<br/>
         Dados pegos do site Themoviedb.org
       </footer>
